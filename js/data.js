9d7006405a54d03ddb58c2a0ddc74a1c010df0c1
GitHub Action <action@github.com>
2025-07-21 17:29:22 +0000

    auto: update scraped data

--- js/data.js
+++ js/data.js
@@ -1,41 +1,23 @@
-window.designConferences = [
+window.designConferences = 
+[
   {
-    name: "ICED",
-    fullName: "International Conference on Engineering Design",
-    year: 2023,
-    location: "Bordeaux, France",
-    url: "https://iced2023.sciencesconf.org",
-    startDate: "2023-07-24",
-    endDate: "2023-07-28",
-    proceedings: "https://doi.org/10.1017/abc123"
+    "name": "CIRP Design",
+    "fullName": "36th CIRP Design Conference",
+    "year": null,
+    "location": "Unbekannt",
+    "url": "https://www.cirp.net/event-detail/icalrepeat.detail/2026/03/16/474/-/36th-cirp-design-conference.html",
+    "startDate": null,
+    "endDate": "2026-03-16",
+    "proceedings": null
   },
   {
-    name: "CIRP Design",
-    fullName: "CIRP International Conference on Design",
-    year: 2023,
-    location: "Ghent, Belgium",
-    url: "https://www.cirpdesign2023.be",
-    startDate: "2023-05-10",
-    endDate: "2023-05-12",
-    proceedings: null
-  },
-  {
-    name: "DESIGN",
-    fullName: "Design Society Biennial Conference DESIGN",
-    year: 2022,
-    location: "Cavtat-Dubrovnik, Croatia",
-    url: "https://design2022.foi.hr",
-    startDate: "2022-05-23",
-    endDate: "2022-05-26",
-    proceedings: "https://doi.org/10.1000/design2022"
-  },
-  {
-    name: "NordDesign",
-    year: 2022,
-    location: "Copenhagen, Denmark",
-    url: "https://norddesign2022.dk",
-    startDate: "2022-08-16",
-    endDate: "2022-08-18",
-    proceedings: null
+    "name": "CIRP Design",
+    "fullName": "38th CIRP Design Conference",
+    "year": null,
+    "location": "Unbekannt",
+    "url": "https://www.cirp.net/event-detail/icalrepeat.detail/2028/05/31/2131/-/38th-cirp-design-conference.html",
+    "startDate": null,
+    "endDate": "2028-05-31",
+    "proceedings": null
   }
-];
+]
